0b9530029a33b8b738ebee44790a62f489c7024d
dacrands <davecrands@gmail.com>
2020-09-05 15:26:05 -0500

    Initial UI

--- App.js
+++ App.js
@@ -1,48 +1,74 @@
 import React from 'react';
-import { Text, View } from 'react-native'
+import { Text, Button } from 'react-native'
 import styled  from 'styled-components/native'
+
+const colors = {
+  /* Color Theme Swatches in Hex */
+  veryDarkPurple: '#6A0DC4',
+  darkPurple: '#8D18FF',
+  purple: '#A240FC',
+  lightPurple: '#B850FF',
+  veryLightPurple: '#D168FF'
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <MainView>
-        <Text>This Is Really Cool</Text>
+        <HeaderText>Gratitude</HeaderText>
+        <Input
+          placeholder="What are you thankful for"
+        />
+        <Submit>
+          <SubmitText>Submit</SubmitText>
+        </Submit>
       </MainView>
     );
   }
 }
 
+const Input = styled.TextInput`
+  background: ${colors.veryLightPurple};
+  height: 50px;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 18;
+  color: white;
+`
+
+const HeaderText = styled.Text`
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 4px;
+  font-weight: bold;
+  color: ${colors.lightPurple};
+  font-size: 28;
+`
+
 const MainView = styled.View`
-  background: tan;
+  background: ${colors.veryDarkPurple};
   padding-top: 50px;
   flex: 1;
 `
 
-const LoginView = styled.View`
-  color: #EDFAFD;
-  flex: 1;
-  background: #042291;
-  justify-content: center;
+const Submit = styled.TouchableOpacity`
+  background: orange;
+  display: flex;
   align-items: center;
-`
-
-const LoginInput = styled.TextInput`
-  background: white;
-  width: 40%;
-  margin-bottom: 8px;
-  font-size: 20px;
-  padding: 5px;
-`
-
-const Submit = styled.TouchableHighlight`
-  background: #3DDAD7;
-  /* padding: 5%; */
-
+  margin: 10px;
+  padding: 10px;
+  border-radius: 5px;
 `
 
 const SubmitText = styled.Text`
-  color: #f9f9f9;
-  font-size: 20;
-  padding: 5px;
+  color: ${colors.purple};
+  font-size: 18;
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-weight: bold;
 `
+
+
 
