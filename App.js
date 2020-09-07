d77add78fb4a8ce0655b023e9e6ab850a905f0cf
dacrands <davecrands@gmail.com>
2020-09-06 21:15:20 -0500

    Remove list item by id

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Keyboard, Alert } from 'react-native'
+import { Keyboard, Alert, Text } from 'react-native'
 import styled  from 'styled-components/native'
 import _uniqueId from 'lodash/uniqueId';
 
@@ -18,12 +18,18 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.renderItem = this.renderItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
     this.clearData = this.clearData.bind(this);
     this.clearAlert = this.clearAlert.bind(this);
     this.textInput = React.createRef();
     this.state = {
       currItem: "",
-      data: []
+      data: [
+        {title: "test",
+        id: 1234},
+        {title: "test2",
+        id: 1235},
+      ]
     }
   }
 
@@ -44,7 +50,7 @@ export default class App extends React.Component {
   ))
 
   renderItem = ({ item }) => (
-    <Item title={item.title} />
+    <Item title={item.title} id={item.id} removeItem={this.removeItem} />
   )
 
   clearData = (() => (
@@ -54,6 +60,12 @@ export default class App extends React.Component {
   addItem = ({ item }) => (
     this.setState({ data: this.state.data.push(item) })
   )
+
+  removeItem = ((id) => (
+    this.setState({
+      data: this.state.data.filter(item => item.id != id)
+    })
+  ))
 
   render() {
     return (
@@ -85,7 +97,7 @@ export default class App extends React.Component {
         <MainList 
           data={this.state.data}
           renderItem={this.renderItem}
-          keyExtractor={item => item.id}
+          keyExtractor={item => item.id.toString()}
         />
         <BigBtn onPress={this.clearAlert}>
           <SubmitText>Clear List</SubmitText>
@@ -95,8 +107,15 @@ export default class App extends React.Component {
   }
 }
 
-const Item = ({ title }) => (
-  <ListText>{title}</ListText>
+const Item = ({ title, id, removeItem }, ) => (
+  <ListView>
+    <ListText>
+      {title}
+    </ListText>
+    <CloseBtn onPress={() => removeItem(id)}>
+      <Text>{"\u274C"}</Text>
+    </CloseBtn>
+  </ListView>
 );
 
 const Input = styled.TextInput`
@@ -106,15 +125,20 @@ const Input = styled.TextInput`
   padding: 10px;
   border-radius: 5px;
   font-size: 18;
-  color: white;
+  color: yellow;
 `
 
-
-const ListText = styled.Text`
-  color: yellow;
+const ListView = styled.View`
+  display: flex;
+  flex-direction: row;
   background: ${colors.purple};
   margin-bottom: 5px;
   padding: 8px;
+`
+
+const ListText = styled.Text`
+  color: yellow;
+  flex-grow: 1;
 `
 
 const HeaderText = styled.Text`
@@ -124,6 +148,7 @@ const HeaderText = styled.Text`
   font-weight: bold;
   color: ${colors.lightPurple};
   font-size: 28;
+  margin-bottom: 20px;
 `
 
 const MainList = styled.FlatList`
@@ -159,5 +184,9 @@ const SubmitText = styled.Text`
   font-weight: bold;
 `
 
+const CloseBtn = styled.TouchableHighlight`
+  color: yellow;
+  width: 20px;
+`
 
 
