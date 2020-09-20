d71ec509d2bfbd47a397c1c7e25c5dc2a4d61550
dacrands <davecrands@gmail.com>
2020-09-19 21:20:33 -0500

    Remove colors file

--- assets/colors.js
+++
@@ -1,9 +0,0 @@
-export default COLORS = {
-    /* Color Theme Swatches in Hex */
-    veryDarkPurple: '#6A0DC4',
-    darkPurple: '#8D18FF',
-    purple: '#A240FC',
-    lightPurple: '#B850FF',
-    veryLightPurple: '#D168FF'
-}
-  
