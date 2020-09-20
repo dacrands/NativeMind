03c30e1d65b5be8710ccc209108067be73a64425
dacrands <davecrands@gmail.com>
2020-09-20 15:39:10 -0500

    Move hompage to separate component

--- App.js
+++ App.js
@@ -1,138 +1,46 @@
-import React from "react";
-import { Keyboard, Alert, View } from "react-native";
-import { Header, SubmitText, MainView, BigBtn, Input } from "./components/Main";
-import { ItemList, Item } from "./components/ListItems";
-import AsyncStorage from "@react-native-community/async-storage";
-import { LinearGradient } from 'expo-linear-gradient';
+import * as React from 'react';
+import Profile from "./components/Profile"
+import { TouchableHighlight } from "react-native"
+import { Header } from "./components/Main"
+import HomeScreen from "./components/Home"
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { Ionicons } from '@expo/vector-icons';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.renderItem = this.renderItem.bind(this);
-    this.removeItem = this.removeItem.bind(this);
-    this.clearData = this.clearData.bind(this);
-    this.getData = this.getData.bind(this);
-    this.storeData = this.storeData.bind(this);
-    this.textInput = React.createRef();
-    this.state = {
-      currItem: "",
-      data: []
-    };
-  }
+const Stack = createStackNavigator();
 
-  async componentDidMount() {
-    const currData = await this.getData("data");
-    if (currData !== null) {
-      this.setState({ data: currData });
-      return;
-    } else {
-      console.log("setting state");
-      this.storeData("data", []);
-    }
-  }
-
-  getData = async key => {
-    try {
-      const jsonValue = await AsyncStorage.getItem(key);
-      return jsonValue != null ? JSON.parse(jsonValue) : null;
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  storeData = async (key, value) => {
-    try {
-      const jsonValue = JSON.stringify(value);
-      await AsyncStorage.setItem(key, jsonValue);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  clearData = () =>
-    Alert.alert(
-      "Clear List",
-      "Are you sure you want to clear your list?",
-      [
-        {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel"
-        },
-        { text: "OK", 
-          onPress: () => {
-            this.setState({ data: [] }) 
-            this.storeData("data", []);
-          }
-        }
-      ],
-      { cancelable: false }
-    );
-
-  renderItem = ({ item }) => (
-    <Item title={item.title} id={item.id} removeItem={this.removeItem} />
-  );
-
-  addItem = async () => {
-    this.setState(state => {
-      Keyboard.dismiss();
-      if (state.currItem === "") {
-        alert("Please enter a value");
-        return;
-      }
-      const data = this.state.data.concat({
-        id: new Date().getTime(),
-        title: state.currItem
-      });
-      // Set storage here to avoid race condition
-      this.storeData("data", data);
-      return {
-        data,
-        currItem: ""
-      };
-    });
-  };
-
-  removeItem = id =>
-    this.setState({
-      data: this.state.data.filter(item => item.id != id)
-    });
-
-  render() {
-    return (
-      <LinearGradient colors={['#38ef7d', '#11998e']} style={{
-        padding: 10,
-        paddingTop: 50,
-        paddingBottom: 80,
-        flex: 1
-        }}>
-        <Header>Gratitude</Header>
-        <View style={{borderBottomColor: '#ffffff',
-                borderBottomWidth: 2,
-                marginBottom: 20,
-                marginLeft: 10,
-                marginRight: 10,
-                }}>
-          <Input
-            ref={this.textInput}
-            placeholder="What are you thankful for"
-            onChangeText={currItem => this.setState({ currItem })}
-            onSubmitEditing={this.addItem}
-            value={this.state.currItem}
-          />
-        </View>
-        <BigBtn onPress={this.addItem}>
-          <SubmitText>Submit</SubmitText>
-        </BigBtn>
-        <ItemList
-          data={this.state.data}
-          renderItem={this.renderItem}
-          keyExtractor={item => item.id.toString()}
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={({ navigation }) => ({ 
+            headerTitle: () => <Header>Gratitude</Header>,
+            headerStyle: {
+              backgroundColor: '#38ef7d'
+            },
+            headerRight: () => (
+              <TouchableHighlight
+                onPress={() => navigation.navigate('Profile')}
+                title="Info"
+                color="#fff"
+                style={{ paddingRight:10 }}
+              >
+                <Ionicons name="md-list" size={32} color="white" />
+              </TouchableHighlight>
+            ),
+           })}
         />
-        <BigBtn onPress={this.clearData}>
-          <SubmitText>Clear List</SubmitText>
-        </BigBtn>
-      </LinearGradient>
-    );
-  }
-}
+        <Stack.Screen 
+          name="Profile"
+          component={Profile}
+          options={{ title: 'Profile' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
