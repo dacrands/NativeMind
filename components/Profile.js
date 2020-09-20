46768b13817291df681aa6b9893f3dd50e35c86d
dacrands <davecrands@gmail.com>
2020-09-20 15:36:58 -0500

    Add Profile component

---
+++ components/Profile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text, View } from "react-native";
+import styled from "styled-components/native";
+
+export default Profile = () => (
+    <View>
+        <Text>This is a test</Text>
+    </View>
+)
