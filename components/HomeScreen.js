fbc7458af6006ee873779c2ff204d1a1c8a59e51
dacrands <davecrands@gmail.com>
2020-10-03 19:00:02 -0500

    Remove console log

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -27,7 +27,6 @@ class HomeScreen extends React.Component {
       this.setState({ data: currData });
       return;
     } else {
-      console.log("setting state");
       storeData("data", []);
     }
   }
