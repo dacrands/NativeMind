52ba1f663814ff6423dc3cb304e2069e61de370f
dacrands <davecrands@gmail.com>
2020-10-05 15:34:21 -0500

    Add HomeScreen input change test

--- __tests__/components/homescreen.test.js
+++ __tests__/components/homescreen.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import renderer, { act } from 'react-test-renderer';
-
-import HomeScreen from '../../src/components/HomeScreen';
+import { mount } from 'enzyme'
+import { HomeScreen } from '../../src/components/HomeScreen';
+import { Input } from "../../src/styles/Main";
 
 jest.mock('@react-navigation/native'); 
 jest.useFakeTimers()
@@ -9,11 +10,21 @@ jest.useFakeTimers()
 describe('<HomeScreen />', () => {
     it('has 5 children', async () => {
         const tree = renderer.create(<HomeScreen />).toJSON();
-        console.log(tree)
         await act(
             async () => {
                 expect(tree.children.length).toBe(5);
             }
         )
+    });
+
+    it('Input change should update state.currItem', async () => {
+        const component = mount(<HomeScreen/>)
+        component
+            .find('TextInput')
+            .hostNodes()
+            .props()
+            .onChangeText('test')
+        const currItem = component.find(HomeScreen).state().currItem;
+        expect(currItem).toEqual('test');
     });
 });
