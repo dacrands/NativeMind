7d16f2762f4ad8cae47e3fd9549bacefa66cd8b0
dacrands <davecrands@gmail.com>
2020-11-03 20:51:27 -0600

    Add SavedLists test

---
+++ __tests__/components/saved_lists.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import renderer, { act } from 'react-test-renderer';
+import { mount } from 'enzyme'
+import SavedLists  from '../../src/components/SavedLists';
+import { Alert } from 'react-native';
+
+
+describe('<SavedLists />', () => {
+    it('has 5 children', async () => {
+        const tree = renderer.create(<SavedLists />).toJSON();
+        await act(
+            async () => {
+                expect(tree.children.length).toBe(1);
+            }
+        )
+    });
+})
