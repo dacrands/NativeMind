ef0b8560e8cd3fa6562e1a2694937a2b37c53a5e
dacrands <davecrands@gmail.com>
2020-11-25 19:51:32 -0600

    Add cancel delete item test

--- __tests__/components/saved_lists.test.js
+++ __tests__/components/saved_lists.test.js
@@ -4,6 +4,10 @@ import { mount } from 'enzyme'
 import SavedLists  from '../../src/components/SavedLists';
 import { Alert } from 'react-native';
 
+jest.mock('@react-navigation/native'); 
+jest.useFakeTimers()
+jest.spyOn(Alert, 'alert')
+jest.spyOn(window, 'alert').mockImplementation(() => {});
 
 describe('<SavedLists />', () => {
     it('has 1 child', async () => {
@@ -13,5 +17,25 @@ describe('<SavedLists />', () => {
                 expect(tree.children.length).toBe(1);
             }
         )
+    });
+
+    it('Does not delete list if delete cancelled', async () => {
+        const component = mount(<SavedLists/>)
+        const testList = [{
+            title: 'Test Title',
+            list: [ { id: 2, title: "Test"} ],
+            id: 1
+        }]
+
+        component.setState({ lists: testList })
+
+        component
+            .find(`#delete-list-${testList[0].id}-btn`)
+            .at(1)
+            .props()
+            .onPress()
+
+        Alert.alert.mock.calls[0][2][0].onPress()
+        expect(component.find(SavedLists).state().lists).toEqual(testList)
     });
 })
